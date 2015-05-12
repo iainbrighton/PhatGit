778a811d84173e980067cb761fb8de00444245a9
Iain Brighton <iainbrighton@hotmail.com>
2015-05-12 17:21:45 +0100

    Removed PhatGit variable definition from PhatGit.ps1

--- PhatGit.ps1
+++ PhatGit.ps1
@@ -1,20 +1,5 @@
 Import-LocalizedData -BindingVariable localizedData -FileName Resources.psd1;
 
-## Set default global known commands. These can be overridden in a user's profile as required.
-## NOTE: It's important to specify matching command parameters BEFORE missing parameters!
-$knownGitCommands = @(
-    @{ Command = 'commit'; Parameter = '--interactive'; Exists = $true; MessageId = 'InteractiveCommitMessageWarning'; }
-    @{ Command = 'commit'; Parameter = '-m'; Exists = $false; MessageId = 'MissingCommitMessageWarning'; }
-)
-$ignoredGitCommands = @(
-    @{ Command = 'push'; }
-    @{ Command = 'pull'; }
-    @{ Command = 'clone'; }
-)
-## Set the variables in the parent scope.
-Set-Variable -Name PhatGitKnownCommands -Value $knownGitCommands -Scope 1 -Visibility Public;
-Set-Variable -Name PhatGitIgnoredCommands -Value $ignoredGitCommands -Scope 1 -Visibility Public;
-
 function Invoke-PhatGit {
     <#
     .SYNOPSIS
