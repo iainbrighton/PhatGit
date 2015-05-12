c654f0608dc42bdf40c22495034d9fa06d6f404e
Iain Brighton <iainbrighton@hotmail.com>
2015-05-12 10:23:19 +0100

    Fixed string escaping and minor layout changes

--- PhatGit.psm1
+++ PhatGit.psm1
@@ -16,28 +16,24 @@ function Invoke-PhatGit {
     param (
         [Parameter(ValueFromRemainingArguments = $true)] $parameters
     )
-
     process {
-
-        if (-not([string]::IsNullOrEmpty($MyInvocation.PSCommandPath)) -or
-                -not($Host.Name.Contains('ISE'))) {
+        if (-not ([string]::IsNullOrEmpty($MyInvocation.PSCommandPath)) -or
+                -not ($Host.Name.Contains('ISE'))) {
                     ## We're not running interactively or not in the ISE so launch native
                     ## 'git' so that any existing tooling behaves as expected, e.g. posh-git etc.
                     $commandParameters = $parameters -Join ' ';
                     Invoke-Expression "Git.exe $commandParameters";
         }
         else {
-
             ## Otherwise, redirect output streams so they can be echoed nicely
             Write-Verbose "Redirecting output streams.";
-
             ## Re-quote any parameters with spaces, e.g. git commit -m "commit message"
             for ($i = 0; $i -lt $parameters.Count; $i++) {
                 ##TODO: Errors when running 'git log -n 2' instead of 'git log -n2'
-                if (-not([string]::IsNullOrEmpty($parameters[$i])) -and $parameters[$i].Contains(' ')) {
-                    $parameters[$i] = "`"$($parameters[$i])`""; }
-            }
-
+                if (-not ([string]::IsNullOrEmpty($parameters[$i])) -and $parameters[$i].Contains(' ')) {
+                    $parameters[$i] = '"{0}"' -f $parameters[$i];
+                }
+            } #end for
             $processStartInfo = New-object System.Diagnostics.ProcessStartInfo;
             $processStartInfo.CreateNoWindow = $false;
             $processStartInfo.UseShellExecute = $false;
@@ -46,16 +42,13 @@ function Invoke-PhatGit {
             $processStartInfo.Arguments = $parameters;
             $processStartInfo.RedirectStandardOutput = $true;
             $processStartInfo.RedirectStandardError = $true;
-
             $process = [System.Diagnostics.Process]::Start($processStartInfo);
-            Write-Debug ("Started process '{0}'." -f $process.Id);
-
+            Write-Debug ('Started process ''{0}''.' -f $process.Id);
             ## Launch the process and wait for up to 3 seconds
             $exitedCleanly = $process.WaitForExit(3000);
-
             if (-not($exitedCleanly)) {
-                Write-Warning ("Process 'git {0}' did not exit cleanly. Probably waiting for user input?" -f ($parameters -join ' '));
-                Write-Warning ("Stopping process '{0}'." -f $process.Id);
+                Write-Warning ('Process ''git {0}'' did not exit cleanly. Probably waiting for user input?' -f ($parameters -join ' '));
+                Write-Warning ('Stopping process ''{0}''.' -f $process.Id);
                 Stop-Process -Id $process.Id -Force;
             }
             else {
@@ -68,7 +61,6 @@ function Invoke-PhatGit {
                     if (-not([string]::IsNullOrEmpty($errorOutput))) { Write-Host $errorOutput -ForegroundColor $errorForegroundColor -NoNewline; }
                 }
             }
-
         } # end else
     } # end process
 } # end function Invoke-PhatGit
@@ -102,16 +94,12 @@ function ConvertToConsoleColor {
         [ValidateSet('Black','DarkBlue','DarkGreen','DarkCyan','DarkRed','DarkMagenta','DarkYellow','Gray','DarkGray','Blue','Green','Cyan','Red','Magenta','Yellow','White')]
         [System.String] $DefaultColor = 'Red'
     )
-
     begin {
-        Write-Debug ("Using parameter set '$($PSCmdlet.ParameterSetName)'.");
+        Write-Debug ('Using parameter set ''{0}''.' -f $PSCmdlet.ParameterSetName);
     }
-
     process {
-
         foreach ($consoleColor in [enum]::GetValues([System.ConsoleColor])) {
             $color = [System.Drawing.Color]::$consoleColor;
-
             switch ($PSCmdlet.ParameterSetName) {
                 {$_ -eq 'Color'} {
                     $R = $InputObject.R;
@@ -119,23 +107,19 @@ function ConvertToConsoleColor {
                     $B = $InputObject.B;
                 }
             } # end switch
-
             ## Do we have a match (not checking the 'A' value)?
             if (($color.R -eq $R) -and
                     ($color.G -eq $G) -and
                         ($color.B -eq $B)) {
                 return $color.Name;
             }
-
         } # end foreach
-
         ## Check for DarkYellow as this isn't a [System.ConsoleColor] enum?!
         if (($color.R -eq 128) -and
                 ($color.G -eq 128) -and
                     ($color.B -eq 0)) {
             return "DarkYellow";
         }
-
         ## If we've got here we don't have a match so return the default.
         return $DefaultColor;
     } # end process
